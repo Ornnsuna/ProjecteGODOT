938c9001bab84328fb70dd1a3c13180b145dd2ae
Alfred <115370004+Ornnsuna@users.noreply.github.com>
2024-10-18 15:55:26 +0200

    Creació Script Jugador

---
+++ PrimerProjecte/Player.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+
+# Variables:
+export var speed = 400 # A quina velocitat es mourà el jugador (píxels/seg).
+var screan_size # Mida de la finestra del joc
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
