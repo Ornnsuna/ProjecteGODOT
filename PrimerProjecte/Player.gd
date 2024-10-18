6a158835522bee0ba0b25849f23f6b17983e169b
Alfred <115370004+Ornnsuna@users.noreply.github.com>
2024-10-18 16:01:06 +0200

    Assignació de tecles

--- PrimerProjecte/Player.gd
+++ PrimerProjecte/Player.gd
@@ -3,12 +3,12 @@ extends Area2D
 
 # Variables:
 export var speed = 400 # A quina velocitat es mourà el jugador (píxels/seg).
-var screan_size # Mida de la finestra del joc
+var screen_size # Mida de la finestra del joc
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	screen_size = get_viewport_rect().size
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
