b01381f511690689e02efaa4a1831638cc370792
Alfred <115370004+Ornnsuna@users.noreply.github.com>
2024-11-15 16:30:16 +0100

    Afegim sons

--- PrimerProjecte/main.gd
+++ PrimerProjecte/main.gd
@@ -22,12 +22,15 @@ func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 	
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start() #Replace with function body.
 	get_tree().call_group("mobs", "queue_free")
+	$Music.play()
 
 func _on_MobTimer_timeout():
 		# Crea una nova instància de l'escena Mob.
