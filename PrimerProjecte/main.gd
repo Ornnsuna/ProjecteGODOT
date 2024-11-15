493860d64a217e7227a3f38c90ea93f8ede45448
Alfred <115370004+Ornnsuna@users.noreply.github.com>
2024-11-15 15:58:11 +0100

    Joc jugable V1

--- PrimerProjecte/main.gd
+++ PrimerProjecte/main.gd
@@ -11,7 +11,7 @@ var score
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
-
+	new_game()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -29,14 +29,30 @@ func new_game():
 
 
 func _on_MobTimer_timeout():
-	pass # Replace with function body.
+		# Crea una nova instància de l'escena Mob.
+	var mob = mob_scene.instance()
+	# Trieu una ubicació aleatòria a Path2D.
+	var mob_spawn_location = get_node("MobPath/MobSpawnLocation")
+	mob_spawn_location.offset = randi()
+	# Estableix la direcció de la multitud perpendicular a la direcció del camí.
+	var direction = mob_spawn_location.rotation + PI / 2
+	# Estableix la posició de la multitud a una ubicació aleatòria.
+	mob.position = mob_spawn_location.position
+	# Afegeix una mica d'aleatorietat a la direcció.
+	direction += rand_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+	# Tria la velocitat de la multitud.
+	var velocity = Vector2(rand_range(150.0, 250.0), 0.0)
+	mob.linear_velocity = velocity.rotated(direction)
+	# Va fer que la multitud s'afegís a l'escena principal.
+	add_child(mob)
 
+func update_score(score):
+	 $ScoreLabel.text = str(score)
 
 func _on_ScoreTimer_timeout():
 	score += 1 #Replace with function body.
 
-
-
-
 func _on_StartTimer_timeout():
-	pass # Replace with function body.
+	$MobTimer.start()
+	$ScoreTimer.start()
